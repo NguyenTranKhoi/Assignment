5f634dee8d2897a564d00db6149601e3fe1c8e64
khoi <khoidolce@gmail.com>
2022-02-13 19:02:48 +0700

    update home

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Navigo from "navigo";
 import Home from "./pages/home";
 
-const router = new Navigo("/", { linksSelector: "a", has: true });
+const router = new Navigo("/", { linksSelector: "a", hash: true });
 
 const render = async (content, id) => {
     document.querySelector("#app").innerHTML = await content.print(id);
     if (content.afterRender) content.afterRender();
+    console.log(112);
 };
 
 router.on({
     "/": () => render(Home),
 });
+
+router.resolve();
