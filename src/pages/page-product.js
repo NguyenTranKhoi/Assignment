64c583741bcecbd9b11c074f2eef12b603d77644
khoi <khoidolce@gmail.com>
2022-02-20 17:32:26 +0700

    update page product news

---
+++ src/pages/page-product.js
@@ -0,0 +1,11 @@
+import PagePrductNews from "../components/page-prduct-new";
+
+const PageProduct = {
+    print() {
+        return /* html */ `
+            ${PagePrductNews.print()}
+        `;
+    },
+};
+
+export default PageProduct;
